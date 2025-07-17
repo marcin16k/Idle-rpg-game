e808ccacacc3b5bad6c3569ed682bb98c5811715
marcin16k <Metingram25@gmail.com>
2025-07-17 16:00:35 +0100

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,19 +1,13 @@
-export function saveGame(state) {
-  localStorage.setItem("idleRPG", JSON.stringify(state));
+// save.js
+
+function saveGame() {
+  localStorage.setItem("idleRpgData", JSON.stringify(data));
 }
 
-export function loadGame() {
-  const saved = localStorage.getItem("idleRPG");
-  return saved
-    ? JSON.parse(saved)
-    : {
-        gold: 0,
-        level: 1,
-        xp: 0,
-        xpMax: 10,
-        statPoints: 0,
-        stats: { strength: 0, crit: 0 },
-        inventory: [],
-        skills: [],
-      };
+function loadGame() {
+  const saved = localStorage.getItem("idleRpgData");
+  if (saved) {
+    data = JSON.parse(saved);
+  }
+  updateUI();
 }
