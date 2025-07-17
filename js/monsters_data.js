e4a9b1d4c84e80b0c60ad8abbe9e887a8197da8c
marcin16k <Metingram25@gmail.com>
2025-07-17 16:44:00 +0100

    Add files via upload

---
+++ js/monsters_data.js
@@ -0,0 +1,9 @@
+
+const allMonsters = [
+  { name: "Potwór 1", img: "assets/monsters/monster_1.png", hp: 30, attack: 5 },
+  { name: "Potwór 2", img: "assets/monsters/monster_2.png", hp: 40, attack: 7 },
+  { name: "Potwór 3", img: "assets/monsters/monster_3.png", hp: 50, attack: 8 },
+  { name: "Potwór 4", img: "assets/monsters/monster_4.png", hp: 60, attack: 10 },
+  { name: "Potwór 5", img: "assets/monsters/monster_5.png", hp: 75, attack: 12 },
+  { name: "Potwór 6", img: "assets/monsters/monster_6.png", hp: 100, attack: 15 }
+];
